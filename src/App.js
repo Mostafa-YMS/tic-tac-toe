0bcd686b90173daa9da67fd1c8a2abe5184470c4
Mostafa-YMS <mos_302@yahoo.com>
2022-07-29 14:59:39 +0200

    get players

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import Players from "./Pages/Players";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Game from "./Pages/Game";
 
 function App() {
   const [players, setPlayers] = useState();
+  const [loading, setLoading] = useState(true);
   const changePlayers = useCallback((data) => setPlayers(data), []);
 
+  const getPlayers = useCallback(() => {
+    const data = JSON.parse(localStorage.getItem("players"));
+    setPlayers(data);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPlayers && getPlayers();
+  }, [getPlayers]);
+
   return (
-    <div>
-      {players?.firstPlayer && players?.secondPlayer ? (
-        <Game players={players} setPlayers={changePlayers} />
-      ) : (
-        <Players players={players} setPlayers={changePlayers} />
+    <>
+      {!loading && (
+        <div>
+          {players?.firstPlayer && players?.secondPlayer ? (
+            <Game players={players} setPlayers={changePlayers} />
+          ) : (
+            <Players players={players} setPlayers={changePlayers} />
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
