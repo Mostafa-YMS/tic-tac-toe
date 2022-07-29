01f7fb04eace6313d81d434c2be48553bf1ffc9c
Mostafa-YMS <mos_302@yahoo.com>
2022-07-29 15:45:34 +0200

    player sides

---
+++ src/Components/PlayerSide.jsx
@@ -0,0 +1,36 @@
+const mainDivStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+};
+const sideBoxStyle = {
+  width: "75px",
+  height: "75px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "6px",
+};
+
+const sideStyle = { fontSize: "48px", fontWeight: "500" };
+
+const PlayerSide = ({ player, turn, side }) => {
+  return (
+    <div style={{ ...mainDivStyle, color: turn && "#2BA400" }}>
+      <div
+        style={{
+          ...sideBoxStyle,
+          border: `3px solid ${turn ? "#2BA400" : "#000000"}`,
+        }}
+      >
+        <p style={sideStyle}>{side}</p>
+      </div>
+      <p>{`Player ${side === "X" ? 1 : 2} name: ${player?.firstName} ${
+        player?.lastName
+      }`}</p>
+    </div>
+  );
+};
+
+export default PlayerSide;
--- src/Pages/Game/index.jsx
+++ src/Pages/Game/index.jsx
@@ -1,5 +1,29 @@
-const Game = () => {
-  return <div></div>;
+import PlayerSide from "../../Components/PlayerSide";
+import { useState } from "react";
+const Game = ({ players, setPlayers }) => {
+  const [turn, setTurn] = useState("X");
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <PlayerSide
+          player={players?.firstPlayer}
+          side={"X"}
+          turn={turn === "X"}
+        />
+        <PlayerSide
+          player={players?.secondPlayer}
+          side={"O"}
+          turn={turn === "O"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Game;
